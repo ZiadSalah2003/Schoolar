9b99949ae4d5e43e071b7f9d7a9e79ef8a8843de
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-10-03 20:07:24 +0300

    add student repository for get all students

---
+++ Schoolar.infrastructure/Abstracts/IStudentRepository.cs
@@ -0,0 +1,14 @@
+﻿using Schoolar.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schoolar.infrastructure.Abstracts
+{
+	public interface IStudentRepository
+	{
+		public Task<List<Student>> GetStudentsAsync();
+	}
+}
---
+++ Schoolar.infrastructure/Repositories/StudentRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Schoolar.Data.Entities;
+using Schoolar.infrastructure.Abstracts;
+using Schoolar.infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schoolar.infrastructure.Repositories
+{
+	public class StudentRepository : IStudentRepository
+	{
+		private readonly ApplicationDbContext _context;
+		public StudentRepository(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+		public async Task<List<Student>> GetStudentsAsync()
+		{
+			return await _context.Students.Include(x => x.Department).ToListAsync();
+		}
+	}
+}
