f593dfa497071d446c088a6ef4f1721d2e0f5495
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-10-24 14:56:54 +0300

    add localization resources

---
+++ Schoolar.Core/Resources/SharedResourcesKeys.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schoolar.Core.Resources
+{
+	public static class SharedResourcesKeys
+	{
+		public const string Required = "Required";
+		public const string NotFound = "NotFound";
+		public const string Deleted = "Deleted";
+		public const string Created = "Created";
+		public const string Success = "Success";
+		public const string NotEmpty = "NotEmpty";
+		public const string Updated = "Updated";
+		public const string UnAuthorized = "UnAuthorized";
+		public const string BadRequest = "BadRequest";
+		public const string UnprocessableEntity = "UnprocessableEntity";
+		public const string MaxLengthis100 = "MaxLengthis100";
+		public const string IsExist = "IsExist";
+		public const string IsNotExist = "IsNotExist";
+		public const string DepartmementId = "DepartmementId";
+		public const string PasswordNotEqualConfirmPass = "PasswordNotEqualConfirmPass";
+		public const string EmailIsExist = "EmailIsExist";
+		public const string UserNameIsExist = "UserNameIsExist";
+		public const string FaildToAddUser = "FaildToAddUser";
+		public const string UpdateFailed = "UpdateFailed";
+		public const string DeletedFailed = "DeletedFailed";
+		public const string ChangePassFailed = "ChangePassFailed";
+		public const string UserName = "UserName";
+		public const string Password = "Password";
+		public const string UserNameIsNotExist = "UserNameIsNotExist";
+		public const string PasswordNotCorrect = "PasswordNotCorrect";
+	}
+}
