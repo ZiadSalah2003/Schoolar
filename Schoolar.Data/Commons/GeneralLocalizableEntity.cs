2d1efbdf4235175885c6c724c383c32696481830
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-10-26 21:17:02 +0300

    update localication entity

---
+++ Schoolar.Data/Commons/GeneralLocalizableEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Schoolar.Data.Commons
+{
+	public class GeneralLocalizableEntity
+	{
+		public string Localize(string nameAr,string name)
+		{
+			CultureInfo culture = Thread.CurrentThread.CurrentCulture;
+			if (culture.TwoLetterISOLanguageName.ToLower().Equals("ar"))
+				return nameAr;
+			return name;
+		}
+	}
+}
---
+++ Schoolar.Data/Commons/LocalizableEntity.cs
@@ -0,0 +1,23 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Globalization;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Schoolar.Data.Commons
+//{
+//	public class LocalizableEntity
+//	{
+//        public string NameAr { get; set; }
+//        public string Name { get; set; }
+
+//        public string GetLocalizedName()
+//		{
+//			CultureInfo culture = Thread.CurrentThread.CurrentCulture;
+//			if(culture.TwoLetterISOLanguageName.ToLower().Equals("ar"))
+//				return NameAr;
+//			return Name;
+//		}
+//	}
+//}
