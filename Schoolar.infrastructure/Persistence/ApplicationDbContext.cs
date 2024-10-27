dbca4637a2ade54cf94a9ab7b047e91095409333
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-10-27 20:08:41 +0300

    fix composite key and make relations to be Restrict

--- Schoolar.infrastructure/Persistence/ApplicationDbContext.cs
+++ Schoolar.infrastructure/Persistence/ApplicationDbContext.cs
@@ -15,11 +15,22 @@ namespace Schoolar.infrastructure.Persistence
 		public DbSet<Subjects> Subjects { get; set; }
 		public DbSet<DepartmetSubject> DepartmetSubjects { get; set; }
 		public DbSet<StudentSubject> StudentSubjects { get; set; }
-        public ApplicationDbContext()
-        {
-        }
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		public ApplicationDbContext()
 		{
+		}
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+		}
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<DepartmetSubject>().HasKey(x => new { x.DepartmetId, x.SubjectId });
+			modelBuilder.Entity<StudentSubject>().HasKey(x => new { x.StudentId, x.SubjectId });
+			modelBuilder.Entity<InstructorSubject>().HasKey(x => new { x.InstructorId, x.SubjectId });
+
+			modelBuilder.Entity<Instructor>().HasOne(x => x.Supervisor).WithMany(x => x.Instructors).HasForeignKey(x => x.SupervisorId).OnDelete(DeleteBehavior.Restrict);
+			modelBuilder.Entity<Department>().HasOne(x => x.Instructor).WithOne(x => x.departmentManager).HasForeignKey<Department>(x => x.InstructorManger).OnDelete(DeleteBehavior.Restrict);
+
+			base.OnModelCreating(modelBuilder);
 		}
 	}
 }
