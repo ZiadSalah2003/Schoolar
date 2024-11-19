42acbe7aa626083872ab083df68974e63c10a613
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-11-19 20:50:11 +0200

    add department controller and route for controller

---
+++ Schoolar.API/Controllers/DepartmentsController.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Schoolar.API.Bases;
+using Schoolar.Core.Features.Departments.Queries.Models;
+
+using Route = Schoolar.Data.AppMetaData.Route;
+namespace Schoolar.API.Controllers
+{
+	//[Route("api/[controller]")]
+	[ApiController]
+	public class DepartmentsController : AppControllerBases
+	{
+		[HttpGet(Route.DepartmentRouting.GetByID)]
+		public async Task<IActionResult> GetStudentById([FromRoute] int id)
+		{
+			var result = await Mediator.Send(new GetDepartmentByIdQuery(id));
+			return NewResult(result);
+		}
+	}
+}
